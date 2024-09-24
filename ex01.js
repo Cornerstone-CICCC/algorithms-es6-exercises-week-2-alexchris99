e17ed9c794373c77a5d4a45bbe39ed4e39fa92a9
Christian Jimenez <cajt@outlook.com>
2024-09-24 13:07:24 -0800

    Excercise 2

--- ex01.js
+++ ex01.js
@@ -1,13 +1,36 @@
 /*
 In this exercise, we will be given an array of 2 or more numbers. 
-We will then have to find the two largest numbers in that array, and sum them together.
+We will then have to find the two largest numbers in that array, 
+and sum them together.
 
 Instruction
-Create a function named sumLargestNumbers that will receive an array of numbers and return the sum of the two largest numbers in that array.
+Create a function named sumLargestNumbers that will receive an 
+array of numbers and return the sum of the two largest numbers in 
+that array.
 */
 
 const sumLargestNumbers = function (data) {
-  // Put your solution here
+  let num1 = 0
+  let num2 = 0
+  let num3 = 0
+  if(data.length == 2){
+    data.forEach(element => {
+      num1 += element
+    });
+    return num1
+  }
+  else{
+    data.forEach(element => {
+      num1 = 1
+      if(num1 < element){
+        num2 = element
+        console.log(num2)
+      }
+      else{
+        num1 = element
+      }
+    });
+  }
 };
 
 console.log(sumLargestNumbers([1, 10])); // 11
--- ex02.js
+++ ex02.js
@@ -1,13 +1,36 @@
 /*
-We'll be adding only the numbers in the array which match the given condition.
+We'll be adding only the numbers in the array which match 
+the given condition.
 
 Instruction
-Create a function named conditionalSum that will be given an array of numbers and a condition, in this case odd or even. Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
+Create a function named conditionalSum that will be 
+given an array of numbers and a condition, in this case odd or even. Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
 
 */
 
 const conditionalSum = function (values, condition) {
-  // Your code here
+  if(condition == "even"){
+    const evenNum = values.filter(item =>{
+      if(item % 2 == 0){
+        return item;
+      }
+    })
+    const sum = evenNum.reduce((acumulator, currentValue)=>{
+      return acumulator + currentValue
+    },0);
+    return sum
+  }
+  else{
+    const evenNum = values.filter(item =>{
+      if(item % 2 != 0){
+        return item;
+      }
+    })
+    const sum = evenNum.reduce((acumulator, currentValue)=>{
+      return acumulator + currentValue
+    },0);
+    return sum
+  }
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
