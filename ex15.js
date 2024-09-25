64086afc8144e99415f374d4a66790692d27a467
Christian Jimenez <cajt@outlook.com>
2024-09-25 15:10:45 -0800

    Excersice 15

--- ex15.js
+++ ex15.js
@@ -1,7 +1,9 @@
-/*In this exercise, we will be given a list of instructors and we will create a single object to organize them based on their course.
+/*In this exercise, we will be given a list of instructors and we will create a single object 
+to organize them based on their course.
 
 Instruction
-Create a function named organizeInstructors that will receive an array of instructor objects, and will return a new object that has the following format:
+Create a function named organizeInstructors that will receive an array of instructor objects, 
+and will return a new object that has the following format:
 
 {
   CourseName: [instructors]
@@ -9,8 +11,41 @@ Create a function named organizeInstructors that will receive an array of instru
 */
 
 const organizeInstructors = function (instructors) {
-  // Put your solution here
-};
+  let courses = []
+
+  let newArray = {
+  }
+
+  for(let i = 0; i < instructors.length; i++){
+    if(courses.length == 0){
+      courses.push(instructors[i].course)
+    }else if(instructors[i].course in courses){
+
+    }else{
+      courses.push(instructors[i].course)
+    }
+  }
+
+  let uniqueC = [...new Set(courses)]
+
+  for(let i = 0; i < uniqueC.length; i++){
+    newArray[uniqueC[i]] = []
+  }
+
+
+
+  for(let i = 0; i < instructors.length; i++){
+    if(instructors[i].course == "Web"){
+      newArray.Web.push(instructors[i].name)
+    } else if(instructors[i].course == "iOS"){
+      newArray.iOS.push(instructors[i].name)
+    }else{
+      newArray.Blockchain.push(instructors[i].name)
+    }
+  }
+  
+  return newArray
+}
 
 console.log(
   organizeInstructors([
